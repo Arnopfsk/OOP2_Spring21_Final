c5b23840259e732c7c1dbcfda6d67c1893c88084
19-39906-1@student.aiub.edu <19-39906-1@student.aiub.edu>
2021-03-21 23:48:07 +0600

    Transection

---
+++ FLab 1/FLab 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account Fahim = new Saving("Fahim", "101", 5000.0f);
+            Account Sharier = new Current("Sharier", "202", 4000f);
+            Account Khan = new Fixed("Khan", "303", 3000f, 2021, 17);
+            Fahim.ShowInfo();
+            Sharier.ShowInfo();
+            Khan.ShowInfo();
+        }
+    }
+}
